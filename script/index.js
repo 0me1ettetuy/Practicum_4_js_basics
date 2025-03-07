b2025c73776b32dbb48df50c4c8023c7935c4836
0me1ettetuy <Omelettetuy@gmail.com>
2025-03-07 08:27:24 +0600

    project work 5 initial

--- script/index.js
+++ script/index.js
@@ -1,29 +1,77 @@
-let page = document.querySelector('.page');
-let popup = page.querySelector('.popup');
-let closeButton = popup.querySelector('.popup__close');
-let saveButton = popup.querySelector('.popup__submit');
-let editButton = page.querySelector('.profile__edit-button');
-let inputName = popup.querySelector('.popup__input_type_name');
-let inputBio = popup.querySelector('.popup__input_type_bio');
-let profile = page.querySelector('.profile');
-let profileName = profile.querySelector('.profile__name');
-let profileBio = profile.querySelector('.profile__bio');
+const page = document.querySelector('.page');
+const popupEdit = page.querySelector('.popup_type_edit');
+const popupNewImage = page.querySelector('.popup_type_new-image');
+const popupNewImageCloseButton = popupNewImage.querySelector('.popup__close');
+const popupEditCloseButton = popupEdit.querySelector('.popup__close');
+const popupEditSaveButton = popupEdit.querySelector('.popup__submit');
+const popupEditInputName = popupEdit.querySelector('.popup__input_type_name');
+const popupEditInputBio = popupEdit.querySelector('.popup__input_type_bio');
 
-editButton.addEventListener('click', function() {
-  popup.classList.add('popup__opened');
-  inputName.value = profileName.textContent;
-  inputBio.value = profileBio.textContent;
-});
+const profile = page.querySelector('.profile');
+const profileEditButton = page.querySelector('.profile__edit-button');
+const profileAddButton = profile.querySelector('.profile__add-button');
+const profileName = profile.querySelector('.profile__name');
+const profileBio = profile.querySelector('.profile__bio');
 
-closeButton.addEventListener('click', closePopup);
-saveButton.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  profileName.textContent = inputName.value;
-  profileBio.textContent = inputBio.value;
-  closePopup();
-});
+const elementTemplate = page.querySelector('.element-template').content;
+const elements = page.querySelector('.elements');
 
-function closePopup() {
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+function closePopup(popup) {
   popup.classList.remove('popup__opened');
 }
+
+function openPopup(popup) {
+  popup.classList.add('popup__opened');
+}
+
+initialCards.forEach((card) => {
+  const cloneElement = elementTemplate.cloneNode(true);
+  cloneElement.querySelector('.elements__name').textContent = card.name;
+  cloneElement.querySelector('.elements__image').src =  card.link;
+  elements.append(cloneElement);
+});
+
+profileAddButton.addEventListener('click', () => openPopup(popupNewImage));
+popupNewImageCloseButton.addEventListener('click', () => closePopup(popupNewImage));
+
+profileEditButton.addEventListener('click', function() {
+  openPopup(popupEdit);
+  popupEditInputName.value = profileName.textContent;
+  popupEditInputBio.value = profileBio.textContent;
+});
+popupEditCloseButton.addEventListener('click', () => closePopup(popupEdit));
+
+popupEditSaveButton.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  profileName.textContent = popupEditInputName.value;
+  profileBio.textContent = popupEditInputBio.value;
+  closePopup(popupEdit);
+});
 
